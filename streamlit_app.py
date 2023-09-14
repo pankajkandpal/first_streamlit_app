8f564dd7903cd05d98c356700c42842c0e6a9a59
pankajkandpal <pankajkandpal@hotmail.com>
2023-09-15 00:00:45 +0530

    Update streamlit_app.py
    Added the proper description to menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,8 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
-
-streamlit.text('Banana Smoothie')
-streamlit.text('Boiled eggs')
-streamlit.text('Oats')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
