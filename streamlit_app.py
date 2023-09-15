1c44a45de38ff9c3fe58c791f0052da9571f5415
pankajkandpal <pankajkandpal@hotmail.com>
2023-09-16 02:31:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
-
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Favorites')
@@ -52,15 +49,11 @@ except URLError as e:
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + new_fruit + "')")
         return "Thanks for adding " + new_fruit
     #my_fruit_list = my_fruit_list.append(add_my_fruit)
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
-
-if streamlit.button('Add a fruit to the list'):
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
 
 #streamlit.stop()
 
@@ -69,11 +62,19 @@ if streamlit.button('Add a fruit to the list'):
 #my_data_row = my_cur.fetchone()
 #my_data_rows = my_cur.fetchall()
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our Fruit List - Add your favorites!")
+if streamlit.button('Add a fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
+    streamlit.text(back_from_function)
+    
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * from fruit_load_list")
         return my_cur.fetchall()
-if streamlit.button('Get fruit Load List'):
+if streamlit.button('Get Fruit List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
